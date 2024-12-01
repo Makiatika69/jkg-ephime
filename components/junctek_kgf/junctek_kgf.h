f33111b21f5a52e35e1e722fe4c690b9ab8a74d7
Makiatika69 <88628802+Makiatika69@users.noreply.github.com>
2024-12-01 11:47:32 +0100

    Update junctek_kgf.h

--- components/junctek_kgf/junctek_kgf.h
+++ components/junctek_kgf/junctek_kgf.h
@@ -16,7 +16,7 @@ public:
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage_sensor_ = voltage_sensor; }
   void set_current_sensor(sensor::Sensor *current_sensor) { current_sensor_ = current_sensor; }
   void set_temperature_sensor(sensor::Sensor *temperature) { temperature_ = temperature; }
-
+  void set_battery_remain_cap_sensor(sensor::Sensor *battery_remain_cap_sensor) { battery_remain_cap_sensor_ = battery_remain_cap_sensor; }
   void set_battery_level_sensor(sensor::Sensor *battery_level_sensor) { battery_level_sensor_ = battery_level_sensor; }
   void dump_config() override;
   void loop() override;
@@ -38,7 +38,7 @@ protected:
   sensor::Sensor* current_sensor_{nullptr};
   sensor::Sensor* battery_level_sensor_{nullptr};
   sensor::Sensor* temperature_{nullptr};
-
+  sensor::Sensor* battery_remain_cap_sensor_{nullptr};
   static constexpr int MAX_LINE_LEN = 120;
   std::array<char, MAX_LINE_LEN> line_buffer_;
   size_t line_pos_ = 0;
